3220486c84724f261f0060e5d4ef1e3c2b0d117f
hosein <85564616+hoseinalizadehkhiabani@users.noreply.github.com>
2022-05-22 12:15:50 +0430

    twelve

---
+++ js/myjs.js
@@ -0,0 +1,10 @@
+let left = document.getElementById("left-arrow-key");
+let right = document.getElementById("right-arrow-key");
+
+let caontainer_book = document.getElementById("book-list");
+left.addEventListener('click',()=>{
+    caontainer_book.scrollLeft  = 0;
+});
+right.addEventListener('click',()=>{
+caontainer_book.scrollLeft = caontainer_book.scrollWidth;
+});
